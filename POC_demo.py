5ef92a69f6e168b9bb0a44693547603abf1790fb
Darshini Coore <coored@mcmaster.ca>
2024-10-25 18:53:04 -0400

    code to print whether car should move left or right based on RFID tag ID - missing the IDs

--- POC_demo.py
+++ POC_demo.py
@@ -1,1 +1,15 @@
+#RFID TAG ID's
+left = ""
+right = ""
 
+print("STARTING...")
+
+while True:
+    reading = input() #scan RFID tag
+
+    if reading == left:
+        print("move right")
+    elif reading == right:
+        print("move left")
+    else:
+        print("haven't programmed this tag as left or right yet!")
