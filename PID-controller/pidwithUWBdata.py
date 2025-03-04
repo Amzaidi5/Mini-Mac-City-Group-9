26576e1c1d44ff18529435063e30c5703b3b4869
Aaditya Senthilkumar <sentha1@mcmaster.ca>
2025-03-04 17:25:49 -0500

    2ndtag removed

--- PID-controller/pidwithUWBdata.py
+++ PID-controller/pidwithUWBdata.py
@@ -17,26 +17,23 @@ class LaneControl:
         
         # Subscribers for tag distances
         self.offcentre_distance_sub = rospy.Subscriber('/lane_following/offcentre_distance', Float64, self.offcentre_callback) # we need to set this data stream up essentially
-        self.distancefromtag1_sub = rospy.Subscriber('/lane_following/distancefromtag1', Float64, self.tag1_callback) # we need to set this data stream up essentially
-        self.distancefromtag2_sub = rospy.Subscriber('/lane_following/distancefromtag2', Float64, self.tag2_callback) # we need to set this data stream up essentially
+        self.orientation_angle_sub = rospy.Subscriber('/lane_following/orientation', Float64, self.orientation_angle_callback) # we need to set this data stream up essentially
         
         # Publisher for vehicle commands
         self.to_vesc = rospy.Publisher('/ackermann_cmd_mux/input/navigation', AckermannDriveStamped, queue_size=10)
         
         # Error values
         self.offcentre_distance = None # I think you guys have the math to test this out if I am not wrong.
-        self.distancefromtag1 = None
-        self.distancefromtag2 = None
+        self.orientation = None
+        
         
         # PID parameters
         self.kp_steer, self.ki_steer, self.kd_steer = 3, 0.1, 0.05 # we need to test to figure out these values, dummy values for now
-        self.kp_speed, self.ki_speed, self.kd_speed = 3, 0.1, 0.05 # we need to test to figure out these values, dummy values for now
+        #self.kp_speed, self.ki_speed, self.kd_speed = 3, 0.1, 0.05 # we need to test to figure out these values, dummy values for now
         
         # PID states
         self.previous_error_steer = 0
         self.integral_steer = 0
-        self.previous_error_speed = 0
-        self.integral_speed = 0
         
         self.dt = 0.05
         self.rate = rospy.Rate(1 / self.dt) # we need to set this up essentially depending on how often uwb sends data to make it less error prone
@@ -44,27 +41,16 @@ class LaneControl:
     def offcentre_callback(self, msg): 
         self.offcentre_distance = msg.data
     
-    def tag1_callback(self, msg):
-        self.distancefromtag1 = msg.data
-    
-    def tag2_callback(self, msg):
-        self.distancefromtag2 = msg.data
+    def orientation_angle_callback(self, msg):
+        self.orientation_angle = msg.data
     
     def compute_control(self):
         while not rospy.is_shutdown():
             if self.offcentre_distance is None:
                 continue
             
-            # Determine which tag distance to use
-            if self.distancefromtag1 is not None and self.distancefromtag2 is not None:
-                distance_to_use = (self.distancefromtag1 + self.distancefromtag2) / 2  # Kishawn and darshini I need help on how we can convert this to angles. essentially error handling when some tag is not working, but also we can set it up as if either tag is not wor
-              # workking properly we can make changes as needed. 
-            elif self.distancefromtag1 is not None:
-                distance_to_use = self.distancefromtag1
-            elif self.distancefromtag2 is not None:
-                distance_to_use = self.distancefromtag2
-            else:
-                distance_to_use = 0  # Default case when no tag data is available
+            if (self.offcentre_distance<5 and self.offcentre_distance>5)  and self.orientation_angle==0:
+                print("centred")
             
             # Compute steering control
             steering_angle, self.previous_error_steer, self.integral_steer = pid_controller( #figure out exact values later for setpoint
@@ -80,7 +66,7 @@ class LaneControl:
             
             # Compute speed control based on distance from tag
             
-            
+            speed=10 #dummy value for now
             self.drive(speed, -steering_angle) #negative steering angle essentially flips how the car needs to move!! if theres an error just make the change beforehand before sending the value to the vesc.
             self.rate.sleep() # might be a buffer I am not sure.
     
